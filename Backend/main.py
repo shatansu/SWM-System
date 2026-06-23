f280dcd1e6905ab46e51caf69f28aa8d14a72d29
shatansu <pawerkiler1234@gmail.com>
2026-06-23 17:22:32 +0530

    fixing clasyfier

--- Backend/main.py
+++ Backend/main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI, UploadFile, File, Form
 
 from services.scan_services import process_scan
 from services.waste_service import save_report
-
-# -----------------------------
-# Load Environment Variables
-# -----------------------------
 app = FastAPI()
 
 
@@ -13,4 +9,29 @@ app = FastAPI()
 def home():
     return {
         "message": "Eco Visionars Backend Running 🚀"
+    }
+
+@app.post("/scan")
+async def scan(
+    image: UploadFile = File(...),
+    latitude: float = Form(...),
+    longitude: float = Form(...),
+    user_id: str = Form(...)
+):
+
+    # AI Scan
+    ai_result = process_scan(image)
+
+    # MongoDB Save
+    report_id = save_report(
+        user_id=user_id,
+        latitude=latitude,
+        longitude=longitude,
+        image_name=image.filename,
+        ai_result=ai_result
+    )
+
+    return {
+        "report_id": report_id,
+        "ai_result": ai_result
     }
